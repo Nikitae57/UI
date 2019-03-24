a32eaad498b0dcf2294fc85eaf4588cfd77eb4ed
Nikita Evdokimov <nikita-e57@yandex.ru>
2019-03-24 15:01:30 +0300

    Applied right changes to master

--- lab3_1/main.cpp
+++ lab3_1/main.cpp
@@ -6,11 +6,6 @@
 char szClassName[] = "WindowAppClass";
 char buffer[1024];
 
-int mainWindowWidth;
-int mainWindowHeight;
-const int idChild1 = 1001;
-const int idChild2 = 1002;
-
 HINSTANCE hInstance;
 HWND child1Hwnd;
 HWND child2Hwnd;
@@ -46,8 +41,6 @@ LRESULT CALLBACK mainWindowProc(
 ) {
   switch (msg) {
     case WM_CREATE: {
-      child1Hwnd = CreateMDI
-
       child1Hwnd = CreateWindow(
           TEXT("EDIT"),
           (LPSTR) "",
@@ -121,8 +114,6 @@ int WINAPI WinMain(
   MSG msg;
   HWND hwnd;
   hInstance = hInst;
-  mainWindowHeight = GetSystemMetrics(SM_CYSCREEN);
-  mainWindowWidth = GetSystemMetrics(SM_CXSCREEN);
 
   if (!RegClass(mainWindowProc, szClassName, COLOR_WINDOW)) {
     return false;
@@ -147,16 +138,9 @@ int WINAPI WinMain(
 
   if (!hwnd) { return false; }
 
-  child1Hwnd = GetDlgItem(hwnd, idChild1);
-  HACCEL hAccel = 0;
-
   while (GetMessage(&msg, 0, 0, 0) > 0) {
-    if (!TranslateMDISysAccel(child1Hwnd, &msg)
-        && !TranslateAccelerator(hwnd, hAccel, &msg)) {
-
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
   }
 
   return msg.wParam;
