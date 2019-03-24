24ac11504151b6c257e83ae82c975fe44698029b
Nikita Evdokimov <nikita-e57@yandex.ru>
2019-03-24 14:47:50 +0300

    Lab3 finished

--- lab3_1/main.cpp
+++ lab3_1/main.cpp
@@ -72,9 +72,9 @@ LRESULT CALLBACK mainWindowProc(
   switch (msg) {
     case WM_CREATE: {
       child1Hwnd = CreateWindow(
-          childClassname,
-          (LPSTR) "child1",
-          WS_CHILD | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+          TEXT("EDIT"),
+          (LPSTR) "",
+          WS_BORDER|WS_CHILD|WS_VISIBLE |ES_MULTILINE,
           10, 10,
           380, 540,
           hwnd,
@@ -84,9 +84,9 @@ LRESULT CALLBACK mainWindowProc(
       );
 
       child2Hwnd = CreateWindow(
-          childClassname,
-          "child2",
-          WS_CHILD| WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+          TEXT("EDIT"),
+          (LPCSTR) "",
+          WS_BORDER|WS_CHILD|WS_VISIBLE|ES_MULTILINE,
           400, 10,
           380, 540,
           hwnd,
@@ -146,10 +146,6 @@ int WINAPI WinMain(
   hInstance = hInst;
 
   if (!RegClass(mainWindowProc, szClassName, COLOR_WINDOW)) {
-    return false;
-  }
-
-  if (!RegClass(childWindowProc, childClassname, COLOR_WINDOW)) {
     return false;
   }
 
